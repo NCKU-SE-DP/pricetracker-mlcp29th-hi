f86e80686f57ae95c3016cede0b4c45fc5e4e8c8
Student <student@ncku.edu.tw>
2024-11-01 01:02:34 +0800

    refactor: Extract news-related services

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,8 +2,6 @@ import json
 import sentry_sdk
 from apscheduler.schedulers.background import BackgroundScheduler
 from fastapi.middleware.cors import CORSMiddleware
-import itertools
-from sqlalchemy import delete, insert, select
 from typing import List, Optional
 import requests
 from fastapi import APIRouter, HTTPException, Query, Depends, status, FastAPI
@@ -11,11 +9,13 @@ import os
 
 from pydantic import BaseModel, Field, AnyHttpUrl
 
-from .database import get_database, get_database_with_auto_persist_changes_disabled
+from .database import get_database_with_auto_persist_changes_disabled
 from .dependencies import DatabaseSession
-from .models import NewsArticle, user_news_association_table
+from .models import NewsArticle
 from .user.dependencies import CurrentLoggedInUser
 from .user.router import router as user_api_router
+
+from .news import service as news_service
 
 # from pydantic import BaseModel
 
@@ -40,194 +40,23 @@ import os
 from openai import OpenAI
 
 
-# def generate_summary(content):
-#     m = [
-#         {
-#             "role": "system",
-#             "content": "你是一個新聞摘要生成機器人，請統整新聞中提及的影響及主要原因 (影響、原因各50個字，請以json格式回答 {'影響': '...', '原因': '...'})",
-#         },
-#         {"role": "user", "content": f"{content}"},
-#     ]
-#
-#     completion = OpenAI(api_key="xxx").chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         messages=m,
-#     )
-#     return completion.choices[0].message.content
-
-#
-# def extract_search_keywords(content):
-#     m = [
-#         {
-#             "role": "system",
-#             "content": "你是一個關鍵字提取機器人，用戶將會輸入一段文字，表示其希望看見的新聞內容，請提取出用戶希望看見的關鍵字，請截取最重要的關鍵字即可，避免出現「新聞」、「資訊」等混淆搜尋引擎的字詞。(僅須回答關鍵字，若有多個關鍵字，請以空格分隔)",
-#         },
-#         {"role": "user", "content": f"{content}"},
-#     ]
-#
-#     completion = OpenAI(api_key="xxx").chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         messages=m,
-#     )
-#     return completion.choices[0].message.content
-
-
-from urllib.parse import quote
 import requests
 from bs4 import BeautifulSoup
-from sqlalchemy.orm import Session
-
-
-def save_news(news):
-    """
-    save news to database
-    :param news:
-    :return:
-    """
-    session = get_database()
-    session.add(NewsArticle(
-        url=news["url"],
-        title=news["title"],
-        time=news["time"],
-        content=" ".join(news["content"]),  # 將內容list轉換為字串
-        summary=news["summary"],
-        reason=news["reason"],
-    ))
-    session.commit()
-    session.close()
-
-
-def fetch_news_snapshots(search_term, is_initial=False):
-    """
-    fetch news snapshots
-
-    :param search_term:
-    :param is_initial:
-    :return:
-    """
-    news_snapshots = []
-    # iterate pages to get more news data, not actually get all news data
-    if is_initial:
-        snapshots_by_page = []
-        for page in range(1, 10):
-            parameters = {
-                "page": page,
-                "id": f"search:{quote(search_term)}",
-                "channelId": 2,
-                "type": "searchword",
-            }
-            response = requests.get("https://udn.com/api/more", params=parameters)
-            snapshots_by_page.append(response.json()["lists"])
-
-        for snaptshots in snapshots_by_page:
-            news_snapshots.append(snaptshots)
-    else:
-        parameters = {
-            "page": 1,
-            "id": f"search:{quote(search_term)}",
-            "channelId": 2,
-            "type": "searchword",
-        }
-        response = requests.get("https://udn.com/api/more", params=parameters)
-
-        news_snapshots = response.json()["lists"]
-    return news_snapshots
-
-def download_price_changes_news(is_initial=False):
-    """
-    download price changes news
-
-    :param is_initial:
-    :return:
-    """
-    news_snapshots = fetch_news_snapshots("價格", is_initial=is_initial)
-    for snapshot in news_snapshots:
-        title = snapshot["title"]
-        messages = [
-            {
-                "role": "system",
-                "content": "你是一個關聯度評估機器人，請評估新聞標題是否與「民生用品的價格變化」相關，並給予'high'、'medium'、'low'評價。(僅需回答'high'、'medium'、'low'三個詞之一)",
-            },
-            {"role": "user", "content": f"{title}"},
-        ]
-        completion = OpenAI(api_key="xxx").chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-        )
-        relevance = completion.choices[0].message.content
-        if relevance == "high":
-            response = requests.get(snapshot["titleLink"])
-            soup = BeautifulSoup(response.text, "html.parser")
-            # 標題
-            title = soup.find("h1", class_="article-content__title").text
-            time = soup.find("time", class_="article-content__time").text
-            # 定位到包含文章内容的 <section>
-            content_section = soup.find("section", class_="article-content__editor")
-
-            paragraphs = [
-                paragraph.text
-                for paragraph in content_section.find_all("p")
-                if paragraph.text.strip() != "" and "▪" not in paragraph.text
-            ]
-            news =  {
-                "url": snapshot["titleLink"],
-                "title": title,
-                "time": time,
-                "content": paragraphs,
-            }
-            messages = [
-                {
-                    "role": "system",
-                    "content": "你是一個新聞摘要生成機器人，請統整新聞中提及的影響及主要原因 (影響、原因各50個字，請以json格式回答 {'影響': '...', '原因': '...'})",
-                },
-                {"role": "user", "content": " ".join(news["content"])},
-            ]
-
-            completion = OpenAI(api_key="xxx").chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=messages,
-            )
-            summary = completion.choices[0].message.content
-            summary = json.loads(summary)
-            news["summary"] = summary["影響"]
-            news["reason"] = summary["原因"]
-            save_news(news)
-
 
 @app.on_event("startup")
 def start_scheduler():
     database = get_database_with_auto_persist_changes_disabled()
     if database.query(NewsArticle).count() == 0:
         # should change into simple factory pattern
-        download_price_changes_news()
+        news_service.download_price_changes_news()
     database.close()
-    background_scheduler.add_job(download_price_changes_news, "interval", minutes=100)
+    background_scheduler.add_job(news_service.download_price_changes_news, "interval", minutes=100)
     background_scheduler.start()
 
 
 @app.on_event("shutdown")
 def shutdown_scheduler():
     background_scheduler.shutdown()
-
-
-_news_id_counter = itertools.count(start=1000000)
-
-
-def get_upvote_status(news_id, user_id, database):
-    upvote_users_count = (
-        database.query(user_news_association_table)
-        .filter_by(news_articles_id=news_id)
-        .count()
-    )
-    does_user_upvote = False
-    if user_id:
-        does_user_upvote = (
-                database.query(user_news_association_table)
-                .filter_by(news_articles_id=news_id, user_id=user_id)
-                .first()
-                is not None
-        )
-    return upvote_users_count, does_user_upvote
 
 
 @app.get("/api/v1/news/news")
@@ -241,7 +70,7 @@ def read_news(database: DatabaseSession):
     news_list = database.query(NewsArticle).order_by(NewsArticle.time.desc()).all()
     news_list_adding_upvote_status = []
     for news in news_list:
-        upvotes, is_upvoted = get_upvote_status(news.id, None, database)
+        upvotes, is_upvoted = news_service.get_upvote_status(news.id, None, database)
         news_list_adding_upvote_status.append(
             {**news.__dict__, "upvotes": upvotes, "is_upvoted": is_upvoted}
         )
@@ -262,7 +91,7 @@ def read_user_news(database: DatabaseSession, user: CurrentLoggedInUser):
     news_list = database.query(NewsArticle).order_by(NewsArticle.time.desc()).all()
     news_list_adding_upvote_status = []
     for news in news_list:
-        upvotes, is_upvoted = get_upvote_status(news.id, user.id, database)
+        upvotes, is_upvoted = news_service.get_upvote_status(news.id, user.id, database)
         news_list_adding_upvote_status.append(
             {
                 **news.__dict__,
@@ -293,7 +122,7 @@ async def search_news(search_query: SearchRequestSchema):
     )
     keywords = completion.choices[0].message.content
     # should change into simple factory pattern
-    news_snapshots = fetch_news_snapshots(keywords, is_initial=False)
+    news_snapshots = news_service.fetch_news_snapshots(keywords, is_initial=False)
     for snapshot in news_snapshots:
         try:
             response = requests.get(snapshot["titleLink"])
@@ -316,7 +145,7 @@ async def search_news(search_query: SearchRequestSchema):
                 "content": paragraphs,
             }
             news["content"] = " ".join(news["content"])
-            news["id"] = next(_news_id_counter)
+            news["id"] = next(news_service._news_id_counter)
             news_list.append(news)
         except Exception as e:
             print(e)
@@ -355,37 +184,10 @@ def upvote_news(
         database: DatabaseSession,
         user: CurrentLoggedInUser
 ):
-    message = toggle_upvote(id, user.id, database)
+    message = news_service.toggle_upvote(id, user.id, database)
     return {"message": message}
 
 
-def toggle_upvote(news_id, user_id, database):
-    existing_upvote = database.execute(
-        select(user_news_association_table).where(
-            user_news_association_table.c.news_articles_id == news_id,
-            user_news_association_table.c.user_id == user_id,
-        )
-    ).scalar()
-
-    if existing_upvote:
-        deletion_statement = delete(user_news_association_table).where(
-            user_news_association_table.c.news_articles_id == news_id,
-            user_news_association_table.c.user_id == user_id,
-        )
-        database.execute(deletion_statement)
-        database.commit()
-        return "Upvote removed"
-    else:
-        insertion_statement = insert(user_news_association_table).values(
-            news_articles_id=news_id, user_id=user_id
-        )
-        database.execute(insertion_statement)
-        database.commit()
-        return "Article upvoted"
-
-
-def does_news_exist(id, database: Session):
-    return database.query(NewsArticle).filter_by(id=id).first() is not None
 
 
 @app.get("/api/v1/prices/necessities-price")
---
+++ backend/src/news/service.py
@@ -0,0 +1,201 @@
+from bs4 import BeautifulSoup
+import itertools
+import json
+from openai import OpenAI
+import requests
+from sqlalchemy import delete, insert, select
+from sqlalchemy.orm import Session
+from urllib.parse import quote
+from ..database import get_database
+from ..models import NewsArticle, user_news_association_table
+
+
+_news_id_counter = itertools.count(start=1000000)
+
+def save_news(news):
+    """
+    save news to database
+    :param news:
+    :return:
+    """
+    session = get_database()
+    session.add(NewsArticle(
+        url=news["url"],
+        title=news["title"],
+        time=news["time"],
+        content=" ".join(news["content"]),  # 將內容list轉換為字串
+        summary=news["summary"],
+        reason=news["reason"],
+    ))
+    session.commit()
+    session.close()
+
+def fetch_news_snapshots(search_term, is_initial=False):
+    """
+    fetch news snapshots
+
+    :param search_term:
+    :param is_initial:
+    :return:
+    """
+    news_snapshots = []
+    # iterate pages to get more news data, not actually get all news data
+    if is_initial:
+        snapshots_by_page = []
+        for page in range(1, 10):
+            parameters = {
+                "page": page,
+                "id": f"search:{quote(search_term)}",
+                "channelId": 2,
+                "type": "searchword",
+            }
+            response = requests.get("https://udn.com/api/more", params=parameters)
+            snapshots_by_page.append(response.json()["lists"])
+
+        for snaptshots in snapshots_by_page:
+            news_snapshots.append(snaptshots)
+    else:
+        parameters = {
+            "page": 1,
+            "id": f"search:{quote(search_term)}",
+            "channelId": 2,
+            "type": "searchword",
+        }
+        response = requests.get("https://udn.com/api/more", params=parameters)
+
+        news_snapshots = response.json()["lists"]
+    return news_snapshots
+
+def download_price_changes_news(is_initial=False):
+    """
+    download price changes news
+
+    :param is_initial:
+    :return:
+    """
+    news_snapshots = fetch_news_snapshots("價格", is_initial=is_initial)
+    for snapshot in news_snapshots:
+        title = snapshot["title"]
+        messages = [
+            {
+                "role": "system",
+                "content": "你是一個關聯度評估機器人，請評估新聞標題是否與「民生用品的價格變化」相關，並給予'high'、'medium'、'low'評價。(僅需回答'high'、'medium'、'low'三個詞之一)",
+            },
+            {"role": "user", "content": f"{title}"},
+        ]
+        completion = OpenAI(api_key="xxx").chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+        )
+        relevance = completion.choices[0].message.content
+        if relevance == "high":
+            response = requests.get(snapshot["titleLink"])
+            soup = BeautifulSoup(response.text, "html.parser")
+            # 標題
+            title = soup.find("h1", class_="article-content__title").text
+            time = soup.find("time", class_="article-content__time").text
+            # 定位到包含文章内容的 <section>
+            content_section = soup.find("section", class_="article-content__editor")
+
+            paragraphs = [
+                paragraph.text
+                for paragraph in content_section.find_all("p")
+                if paragraph.text.strip() != "" and "▪" not in paragraph.text
+            ]
+            news =  {
+                "url": snapshot["titleLink"],
+                "title": title,
+                "time": time,
+                "content": paragraphs,
+            }
+            messages = [
+                {
+                    "role": "system",
+                    "content": "你是一個新聞摘要生成機器人，請統整新聞中提及的影響及主要原因 (影響、原因各50個字，請以json格式回答 {'影響': '...', '原因': '...'})",
+                },
+                {"role": "user", "content": " ".join(news["content"])},
+            ]
+
+            completion = OpenAI(api_key="xxx").chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=messages,
+            )
+            summary = completion.choices[0].message.content
+            summary = json.loads(summary)
+            news["summary"] = summary["影響"]
+            news["reason"] = summary["原因"]
+            save_news(news)
+
+def get_upvote_status(news_id, user_id, database):
+    upvote_users_count = (
+        database.query(user_news_association_table)
+        .filter_by(news_articles_id=news_id)
+        .count()
+    )
+    does_user_upvote = False
+    if user_id:
+        does_user_upvote = (
+                database.query(user_news_association_table)
+                .filter_by(news_articles_id=news_id, user_id=user_id)
+                .first()
+                is not None
+        )
+    return upvote_users_count, does_user_upvote
+
+def toggle_upvote(news_id, user_id, database):
+    existing_upvote = database.execute(
+        select(user_news_association_table).where(
+            user_news_association_table.c.news_articles_id == news_id,
+            user_news_association_table.c.user_id == user_id,
+        )
+    ).scalar()
+
+    if existing_upvote:
+        deletion_statement = delete(user_news_association_table).where(
+            user_news_association_table.c.news_articles_id == news_id,
+            user_news_association_table.c.user_id == user_id,
+        )
+        database.execute(deletion_statement)
+        database.commit()
+        return "Upvote removed"
+    else:
+        insertion_statement = insert(user_news_association_table).values(
+            news_articles_id=news_id, user_id=user_id
+        )
+        database.execute(insertion_statement)
+        database.commit()
+        return "Article upvoted"
+
+def does_news_exist(news_id, database: Session):
+    return database.query(NewsArticle).filter_by(id=news_id).first() is not None
+
+# def generate_summary(content):
+#     m = [
+#         {
+#             "role": "system",
+#             "content": "你是一個新聞摘要生成機器人，請統整新聞中提及的影響及主要原因 (影響、原因各50個字，請以json格式回答 {'影響': '...', '原因': '...'})",
+#         },
+#         {"role": "user", "content": f"{content}"},
+#     ]
+#
+#     completion = OpenAI(api_key="xxx").chat.completions.create(
+#         model="gpt-3.5-turbo",
+#         messages=m,
+#     )
+#     return completion.choices[0].message.content
+
+#
+# def extract_search_keywords(content):
+#     m = [
+#         {
+#             "role": "system",
+#             "content": "你是一個關鍵字提取機器人，用戶將會輸入一段文字，表示其希望看見的新聞內容，請提取出用戶希望看見的關鍵字，請截取最重要的關鍵字即可，避免出現「新聞」、「資訊」等混淆搜尋引擎的字詞。(僅須回答關鍵字，若有多個關鍵字，請以空格分隔)",
+#         },
+#         {"role": "user", "content": f"{content}"},
+#     ]
+#
+#     completion = OpenAI(api_key="xxx").chat.completions.create(
+#         model="gpt-3.5-turbo",
+#         messages=m,
+#     )
+#     return completion.choices[0].message.content
