3a8a1a9ffda13903be67423961db404df743189d
Student <student@ncku.edu.tw>
2024-11-06 00:13:56 +0800

    refactor: Decouple service from session management

--- backend/src/user/dependencies.py
+++ backend/src/user/dependencies.py
@@ -1,6 +1,21 @@
 from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from src.dependencies import DatabaseSession
 from src.models import User
 from typing import Annotated
-from . import service
+from .config import configuration
 
-CurrentLoggedInUser = Annotated[User, Depends(service.retrieve_user_by_access_token)]
+
+_oauth2_scheme = OAuth2PasswordBearer(tokenUrl=configuration.access_token_url)
+
+
+def _retrieve_user_by_access_token(
+        database: DatabaseSession,
+        token = Depends(_oauth2_scheme)
+):
+    claims = jwt.decode(token, key=configuration.access_token_secret_key, algorithms=[configuration.access_token_algorithm])
+    return database.query(User).filter(User.username == claims.get("sub")).first()
+
+
+CurrentLoggedInUser = Annotated[User, Depends(_retrieve_user_by_access_token)]
--- backend/src/user/service.py
+++ backend/src/user/service.py
@@ -1,15 +1,11 @@
 from datetime import datetime, timedelta
-from fastapi import Depends
-from fastapi.security import OAuth2PasswordBearer
 from jose import jwt
 from passlib.context import CryptContext
-from src.dependencies import DatabaseSession
 from src.models import User
 from .config import configuration
 
 
 _password_context = CryptContext(schemes=[configuration.password_hashing_algorithm], deprecated=["auto"])
-_oauth2_scheme = OAuth2PasswordBearer(tokenUrl=configuration.access_token_url)
 
 
 def hash_password(plaintext: str) -> str:
@@ -25,14 +21,6 @@ def retrieve_user_by_credentials(database, username, password):
     if not is_password_correct(password, user.hashed_password):
         return False
     return user
-
-
-def retrieve_user_by_access_token(
-        database: DatabaseSession,
-        token = Depends(_oauth2_scheme)
-):
-    claims = jwt.decode(token, key=configuration.access_token_secret_key, algorithms=[configuration.access_token_algorithm])
-    return database.query(User).filter(User.username == claims.get("sub")).first()
 
 
 def create_access_token(claims, valid_duration=None):
