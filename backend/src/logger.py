5202b3958b4a2b41321bfa99f6240e82b2a5f29d
Student <student@ncku.edu.tw>
2024-12-26 13:22:33 +0800

    refactor: Wrap logger configuration to a function

--- backend/src/logger.py
+++ backend/src/logger.py
@@ -9,33 +9,37 @@ class Logger:
     def __new__(cls, *args, **kwargs):
         if cls.__shared_instance is None:
             cls.__shared_instance = super().__new__(cls)
-
-            logger = logging.getLogger("pricetracker")
-            logger.setLevel(logging.DEBUG)
-
-            formatter = logging.Formatter(
-                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-            )
-
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            stream_handler.setFormatter(formatter)
-            logger.addHandler(stream_handler)
-
-            file_handler = logging.FileHandler("app.log")
-            file_handler.setLevel(logging.DEBUG)
-            file_handler.setFormatter(formatter)
-            logger.addHandler(file_handler)
-
-            rotating_file_handler = RotatingFileHandler(
-                "app_rotating.log", maxBytes=5 * 1024 * 1024, backupCount=3
-            )
-            rotating_file_handler.setLevel(logging.ERROR)
-            rotating_file_handler.setFormatter(formatter)
-            logger.addHandler(rotating_file_handler)
-
-            cls.__shared_instance._logger = logger
+            cls.__shared_instance._logger = cls.__create_logger()
         return cls.__shared_instance
+
+
+    @staticmethod
+    def __create_logger() -> logging.Logger:
+        logger = logging.getLogger("pricetracker")
+        logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+        file_handler = logging.FileHandler("app.log")
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        rotating_file_handler = RotatingFileHandler(
+            "app_rotating.log", maxBytes=5 * 1024 * 1024, backupCount=3
+        )
+        rotating_file_handler.setLevel(logging.ERROR)
+        rotating_file_handler.setFormatter(formatter)
+        logger.addHandler(rotating_file_handler)
+
+        return logger
 
 
     def log_debug(self, message: object):
